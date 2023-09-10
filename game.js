a5069ee9340a267cfbf6326074294068178f172f
Karuna Sharma <100459894+karunasharma14@users.noreply.github.com>
2023-09-10 17:25:21 +0530

    updated files

--- game.js
+++ game.js
@@ -1,4 +1,84 @@
 
+// var buttonColours = ["red", "blue", "green", "yellow"];
+
+// var gamePattern = [];
+// var userClickedPattern = [];
+
+// var started = false;
+// var level = 0;
+
+// $(document).keypress(function() {
+//   if (!started) {
+//     $("#level-title").text("Level " + level);
+//     nextSequence();
+//     started = true;
+//   }
+// });
+
+// $(".btn").click(function() {
+
+//   var userChosenColour = $(this).attr("id");
+//   userClickedPattern.push(userChosenColour);
+
+//   playSound(userChosenColour);
+//   animatePress(userChosenColour);
+
+//   checkAnswer(userClickedPattern.length-1);
+// });
+
+// function checkAnswer(currentLevel) {
+
+//     if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+//       if (userClickedPattern.length === gamePattern.length){
+//         setTimeout(function () {
+//           nextSequence();
+//         }, 1000);
+//       }
+//     } else {
+//       playSound("wrong");
+//       $("body").addClass("game-over");
+//       $("#level-title").text("Game Over, Press Any Key to Restart");
+
+//       setTimeout(function () {
+//         $("body").removeClass("game-over");
+//       }, 200);
+
+//       startOver();
+//     }
+// }
+
+
+// function nextSequence() {
+//   userClickedPattern = [];
+//   level++;
+//   $("#level-title").text("Level " + level);
+//   var randomNumber = Math.floor(Math.random() * 4);
+//   var randomChosenColour = buttonColours[randomNumber];
+//   gamePattern.push(randomChosenColour);
+
+//   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+//   playSound(randomChosenColour);
+// }
+
+// function animatePress(currentColor) {
+//   $("#" + currentColor).addClass("pressed");
+//   setTimeout(function () {
+//     $("#" + currentColor).removeClass("pressed");
+//   }, 100);
+// }
+
+// function playSound(name) {
+//   var audio = new Audio("sounds/" + name + ".mp3");
+//   audio.play();
+// }
+
+// function startOver() {
+//   level = 0;
+//   gamePattern = [];
+//   started = false;
+// }
+
+
 var buttonColours = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
@@ -7,16 +87,20 @@ var userClickedPattern = [];
 var started = false;
 var level = 0;
 
-$(document).keypress(function() {
+function startGame() {
   if (!started) {
     $("#level-title").text("Level " + level);
     nextSequence();
     started = true;
   }
+}
+
+// Detect both keypress and touchstart events to start the game
+$(document).on("keypress touchstart", function(event) {
+  startGame();
 });
 
-$(".btn").click(function() {
-
+$(".btn").on("click touchstart", function() {
   var userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
 
@@ -27,26 +111,24 @@ $(".btn").click(function() {
 });
 
 function checkAnswer(currentLevel) {
-
-    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
-      if (userClickedPattern.length === gamePattern.length){
-        setTimeout(function () {
-          nextSequence();
-        }, 1000);
-      }
-    } else {
-      playSound("wrong");
-      $("body").addClass("game-over");
-      $("#level-title").text("Game Over, Press Any Key to Restart");
-
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    if (userClickedPattern.length === gamePattern.length) {
       setTimeout(function () {
-        $("body").removeClass("game-over");
-      }, 200);
-
-      startOver();
+        nextSequence();
+      }, 1000);
     }
-}
+  } else {
+    playSound("wrong");
+    $("body").addClass("game-over");
+    $("#level-title").text("Game Over, Press Any Key or Touch Anywhere to Restart");
 
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
+
+    startOver();
+  }
+}
 
 function nextSequence() {
   userClickedPattern = [];
@@ -77,3 +159,8 @@ function startOver() {
   gamePattern = [];
   started = false;
 }
+
+// Prevent default touch events for better mobile user experience
+document.addEventListener("touchstart", function(event) {
+  event.preventDefault();
+});
